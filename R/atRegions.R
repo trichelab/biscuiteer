fddfe579bff0d3e887e069329b4940f4c37171e3
ttriche <tim.triche@gmail.com>
2018-03-23 13:11:37 -0400

    more flexible atRegions/summarizeBsSeqOver behavior

--- R/atRegions.R
+++ R/atRegions.R
@@ -4,12 +4,13 @@
 #' @param regions   a GRanges or GRangesList of regions 
 #' @param mappings  a mapping table, with rownames(mappings) == colnames(bsseq)
 #' @param nm        column of the mapping table to map to (default "POETICname")
+#' @param ...       other arguments to pass on to summarizeBsSeqOver 
 #'
 #' @import bsseq
 #'
 #' @export
-atRegions <- function(bsseq, regions, mappings=NULL, nm="POETICname") { 
-  regional <- summarizeBsSeqOver(bsseq, regions)
+atRegions <- function(bsseq, regions, mappings=NULL, nm="POETICname", ...) { 
+  regional <- summarizeBsSeqOver(bsseq, regions, ...)
   regions <- regions[which(as.character(regions) %in% rownames(regional))]
   regional <- regional[as.character(regions), ]
   rownames(regional) <- names(regions) 
