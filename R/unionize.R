d6d029e83ee87d9b4a7c05ed0a5c982aa6845f1a
ttriche <tim.triche@gmail.com>
2018-04-19 16:37:00 -0400

    parallelize unionize()

--- R/unionize.R
+++ R/unionize.R
@@ -3,11 +3,13 @@
 #' and return the now-even-sparser bsseq holding all of them. All 
 #' conditions governing cbind(bsseq1, bsseq2) also apply to unionize(). 
 #'
-#' @param bs1     a bsseq object (return unaltered if length(list(...)) == 0)
-#' @param ...     one or more bsseq objects to combine with the first one 
+#' @param bs1       a bsseq object (return unaltered if length(list(...)) == 0)
+#' @param ...       one or more bsseq objects to combine with the first one 
+#' @param parallel  split the bsseq objects by chrom and parallelize? (TRUE) 
 #' 
 #' @return        a larger and more sparse bsseq object
 #'
+#' @import parallel 
 #' @importFrom Matrix Matrix
 #' @importClassesFrom Matrix Matrix 
 #' @importMethodsFrom Matrix cbind2 rbind2
@@ -15,7 +17,7 @@
 #' @import bsseq
 #' 
 #' @export
-unionize <- function(bs1, ...) { 
+unionize <- function(bs1, ..., parallel=TRUE) { 
 
   stopifnot(is(bs1, "BSseq"))
   if (length(list(...)) == 0) {
@@ -30,26 +32,36 @@ unionize <- function(bs1, ...) {
   }
 
   # keep only shared
-  message("Pruning...") 
   keptSeqlevels <- intersect(seqlevels(bs1), seqlevels(bs2))
   bs1 <- keepSeqlevels(bs1, keptSeqlevels, pruning.mode="coarse")
   bs2 <- keepSeqlevels(bs2, keptSeqlevels, pruning.mode="coarse")
 
-  # this could be made a lot more efficient by not recursing...
-  message("Merging colData...") 
-  bigpd <- rbind(colData(bs1), colData(bs2))
-  message("Merging rowRanges...") 
-  biggrl <- GRangesList(
-    bs1=suppressWarnings(GenomicRanges::setdiff(granges(bs1), granges(bs2))),
-    both=suppressWarnings(GenomicRanges::intersect(granges(bs1), granges(bs2))),
-    bs2=suppressWarnings(GenomicRanges::setdiff(granges(bs2), granges(bs1)))
-  )
-  message("Merging methylated read matrices...") 
-  M <- unionizeBSseq(bs1, bs2, biggrl, what="M")
-  message("Merging read coverage matrices...") 
-  Cov <- unionizeBSseq(bs1, bs2, biggrl, what="Cov")
+  if (parallel & length(keptSeqlevels) > 1) {
+    unionizeChrom <- function(sub1, sub2) unionize(sub1, sub2, parallel=FALSE)
+    sort(do.call(rbind, 
+                 mcmapply(unionizeChrom, 
+                          split(bs1, seqnames(bs1)),
+                          split(bs2, seqnames(bs2)))))
+  } else { 
+    
+    message("Merging colData...") 
+    bigpd <- rbind(colData(bs1), colData(bs2))
 
-  # sort is required due to the trick in unionizeBSseq
-  sort(BSseq(M, Cov, pData=bigpd, gr=unlist(biggrl)))
+    message("Merging rowRanges...") 
+    biggrl <- GRangesList(
+      bs1=suppressWarnings(GenomicRanges::setdiff(granges(bs1), granges(bs2))),
+      bss=suppressWarnings(GenomicRanges::intersect(granges(bs1),granges(bs2))),
+      bs2=suppressWarnings(GenomicRanges::setdiff(granges(bs2), granges(bs1)))
+    )
+
+    message("Merging methylated read matrices...") 
+    M <- unionizeBSseq(bs1, bs2, biggrl, what="M", parallel=parallel)
+    
+    message("Merging read coverage matrices...") 
+    Cov <- unionizeBSseq(bs1, bs2, biggrl, what="Cov", parallel=parallel)
+    
+    # sort is required due to the trick in unionizeBSseq
+    sort(BSseq(M, Cov, pData=bigpd, gr=unlist(biggrl)))
+  }
 
 }
