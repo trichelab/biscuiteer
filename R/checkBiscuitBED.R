dad181e7d0bf2a4ba7b20dc3526d7e1dcc18b0fa
ttriche <tim.triche@gmail.com>
2018-04-19 15:02:59 -0400

    fix a typo in checkBiscuitBED

--- R/checkBiscuitBED.R
+++ R/checkBiscuitBED.R
@@ -8,6 +8,7 @@
 #' @param chunkSize   for files > `yieldSize` lines long, chunk the file (5e7)
 #' @param merged      boolean; is this merged CpG data? (NULL; guess if merged)
 #' @param hdf5        boolean; use HDF5 arrays for backing of the data? (FALSE)
+#' @param sparse      boolean; use sparse Matrix objects for the data? (TRUE)
 #' 
 #' @return            parameters for makeBSseq or makeBSseq_hdf5
 #'
@@ -21,7 +22,8 @@ checkBiscuitBED <- function(filename,
                             sampleNames=NULL, 
                             chunkSize=5e7, 
                             merged=NULL,
-                            hdf5=FALSE){
+                            hdf5=FALSE,
+                            sparse=TRUE){
 
   if (!base::grepl(".gz$", filename)) stop("Only tabix'ed files are supported.")
   message("Checking ", filename, " for import...")
@@ -122,6 +124,7 @@ checkBiscuitBED <- function(filename,
                  pData=pData,
                  passes=passes,
                  chunkSize=chunkSize,
+                 sparse=sparse,
                  hdf5=hdf5)
   return(params) 
 
