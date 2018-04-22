d15693769a1a984c504bea1a4aeea70d65a316a0
ttriche <tim.triche@gmail.com>
2018-04-22 16:00:02 -0400

    fix defaults for RRBSeq (this turns out to be tricky)

--- R/RRBSeq.R
+++ R/RRBSeq.R
@@ -2,7 +2,8 @@
 #' 
 #' @param bs              a bsseq object
 #' @param testCovariate   the pData column to test on 
-#' @param bpSpan          span of smoother AND max gap in DMR CpGs (500)
+#' @param cutoff          the minimum CpG-wise difference to use (0.2)
+#' @param bpSpan          span of smoother AND max gap in DMR CpGs (750)
 #' @param ...             other arguments to pass along to dmrseq
 #'
 #' @return                a GRanges object (same as from dmrseq)
@@ -10,10 +11,10 @@
 #' @import dmrseq
 #'
 #' @export
-RRBSeq <- function(bs, testCovariate, bpSpan=500, ...) { 
+RRBSeq <- function(bs, testCovariate, cutoff=0.2, bpSpan=750, ...) { 
 
-  dmrseq(filterLoci(bs, testCovariate), testCovariate=testCovariate,
-         bpSpan=bpSpan, maxGap=bpSpan, maxGapSmooth=bpSpan*2, ...)
+  dmrseq(filterLoci(bs, testCovariate), testCovariate=testCovariate, ..., 
+         bpSpan=bpSpan, maxGap=bpSpan, maxGapSmooth=bpSpan*2, cutoff=cutoff)
 
 }
 
