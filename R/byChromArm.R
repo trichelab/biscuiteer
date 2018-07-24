eed61e80e1ed22f1d8b4339f70ade14f73d76581
ttriche <tim.triche@gmail.com>
2018-07-24 10:31:33 -0400

    updates for CpGindex, WGBSage, and so forth

--- R/byChromArm.R
+++ R/byChromArm.R
@@ -1,19 +1,10 @@
 #' an idiotically simple parallelization step that I've been using for years
 #'
-#' Since this is an idiotically simple function, it uses the idiot's favorite 
-#' genome assembly, which is hg19*. If you're going to be a smart person, you
-#' will need to either supply another argument `arms` with those ranges, or 
-#' send in a patch so we can properly support arbitrary genomes (or maybe push
-#' it up the line to GenomeInfoDb for this super generic feature, hint hint).
-#'
 #' This function just splits an object by chromosome arm, which tends to make
 #' parallelization MUCH easier, as cross-arm dependencies are unusual, and the 
 #' larger chromosomes can thus be split across processes or machines without 
 #' worrying much about data starvation for processes on smaller chromosomes.
 #' 
-#' * Note that it's OK to be smart and use hg19_rCRSchrm,
-#'   because mitochondrial chromosomes don't have arms. 
-#'
 #' @param x     anything with a GRanges in it: BSseq, SummarizedExperiment, etc.
 #' @param arms  (optional) another GRanges, but just specifying chromosome arms.
 #'
@@ -22,10 +13,20 @@
 #' @export
 byChromArm <- function(x, arms=NULL) { 
 
-  if (is.null(arms) && (!any(grepl("(GRCh37|hg19)", unique(genome(x)))))) {
-    stop("Your object's genome isn't labeled as GRCh37/hg19 and `arms` is NULL")
+  if (is.null(arms) && 
+      (!any(grepl("(GRCh(37|38)|hg(19|38))", unique(genome(x)))))) {
+    stop("Your object's genome is odd (not hg19/GRCh37/38) and `arms` is NULL.")
   }
 
-  stop("Not quite finished") 
+  if (is.null(arms)) {
+    gome <- unique(genome(x))[1]
+    arms <- paste0(gome, ".chromArm")
+    data(list=arms, package="biscuitEater")
+    arms <- get(arms)
+  }
+
+  ol <- findOverlaps(x, arms)
+  rowRanges(x)$chromArm <- names(arms)[subjectHits(ol)]
+  split(x, rowRanges(x)$chromArm) 
 
 }
