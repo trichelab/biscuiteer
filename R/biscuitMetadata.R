32c2b34572dc0ac3c493f7888738a0cbab425597
ttriche <tim.triche@gmail.com>
2018-08-20 19:31:04 -0400

    use a List for biscuitMetadata output (prettiest option)

--- R/biscuitMetadata.R
+++ R/biscuitMetadata.R
@@ -12,11 +12,8 @@ biscuitMetadata <- function(x) {
     stop("metadata(x)$vcfHeader is NULL -- cannot extract anything from it")
   } else {
     meta <- VariantAnnotation::meta(metadata(x)$vcfHeader)
-    res <- c("Reference genome"=basename(meta$META["reference","Value"]),
-             "Biscuit version"=sub("biscuitV", "", meta$META["source","Value"]),
-             "Invocation"=meta$program[,'cmd'])
-    dat <- DataFrame(Value=res)
-    rownames(dat) <- names(res)
-    return(dat)
+    List("Reference genome"=basename(meta$META["reference","Value"]),
+         "Biscuit version"=sub("biscuitV", "", meta$META["source","Value"]),
+         "Invocation"=meta$program[,'cmd'])
   }
 }
