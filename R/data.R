3bf0e3453c97348bbd63121932918f7875e4b9b3
Morrison <jacob.morrison@submit.cm.cluster>
2019-09-13 11:11:41 -0400

    Initial commit of dataset documentation

---
+++ R/data.R
@@ -0,0 +1,241 @@
+#' ENSR_subset data from hg19 genome
+#'
+#' Dataset description
+#'
+#' @name ENSR_subset.hg19
+#' @docType data
+#' @keywords data
+#' @usage data(ENSR_subset.hg19, package="biscuiteer")
+#'
+NULL
+
+#' ENSR_subset data from hg38 genome
+#'
+#' Dataset description
+#'
+#' @name ENSR_subset.hg38
+#' @docType data
+#' @keywords data
+#' @usage data(ENSR_subset.hg38, package="biscuiteer")
+#'
+NULL
+
+#' hg19.chromArm
+#'
+#' Dataset description
+#'
+#' @name hg19.chromArm
+#' @docType data
+#' @keywords data
+#' @usage data(hg19.chromArm, package="biscuiteer")
+#'
+NULL
+
+#' hg38.chromArm
+#'
+#' Dataset description
+#'
+#' @name hg38.chromArm
+#' @docType data
+#' @keywords data
+#' @usage data(hg38.chromArm, package="biscuiteer")
+#'
+NULL
+
+#' GRCh37.chromArm
+#'
+#' Dataset description
+#'
+#' @name GRCh37.chromArm
+#' @docType data
+#' @keywords data
+#' @usage data(GRCh37.chromArm, package="biscuiteer")
+#'
+NULL
+
+#' GRCh38.chromArm
+#'
+#' Dataset description
+#'
+#' @name GRCh38.chromArm
+#' @docType data
+#' @keywords data
+#' @usage data(GRCh38.chromArm, package="biscuiteer")
+#'
+NULL
+
+#' H9state23unmeth.hg19
+#'
+#' Dataset description
+#'
+#' @name H9state23unmeth.hg19
+#' @docType data
+#' @keywords data
+#' @usage data(H9state23unmeth.hg19, package="biscuiteer")
+#'
+NULL
+
+#' H9state23unmeth.hg38
+#'
+#' Dataset description
+#'
+#' @name H9state23unmeth.hg38
+#' @docType data
+#' @keywords data
+#' @usage data(H9state23unmeth.hg38, package="biscuiteer")
+#'
+NULL
+
+#' HMM_CpG_islands.hg19
+#'
+#' Dataset description
+#'
+#' @name HMM_CpG_islands.hg19
+#' @docType data
+#' @keywords data
+#' @usage data(HMM_CpG_islands.hg19, package="biscuiteer")
+#'
+NULL
+
+#' HMM_CpG_islands.hg38
+#'
+#' Dataset description
+#'
+#' @name HMM_CpG_islands.hg38
+#' @docType data
+#' @keywords data
+#' @usage data(HMM_CpG_islands.hg38, package="biscuiteer")
+#'
+NULL
+
+#' PMDs.hg19
+#'
+#' Dataset description
+#'
+#' @name PMDs.hg19
+#' @docType data
+#' @keywords data
+#' @usage data(PMDs.hg19, package="biscuiteer")
+#'
+NULL
+
+#' PMDs.hg38
+#'
+#' Dataset description
+#'
+#' @name PMDs.hg38
+#' @docType data
+#' @keywords data
+#' @usage data(PMDs.hg38, package="biscuiteer")
+#'
+NULL
+
+#' Zhou_solo_WCGW_inCommonPMDs.hg19
+#'
+#' Dataset description
+#'
+#' @name Zhou_solo_WCGW_inCommonPMDs.hg19
+#' @docType data
+#' @keywords data
+#' @usage data(Zhou_solo_WCGW_inCommonPMDs.hg19, package="biscuiteer")
+#'
+NULL
+
+#' Zhou_solo_WCGW_inCommonPMDs.hg38
+#'
+#' Dataset description
+#'
+#' @name Zhou_solo_WCGW_inCommonPMDs.hg38
+#' @docType data
+#' @keywords data
+#' @usage data(Zhou_solo_WCGW_inCommonPMDs.hg38, package="biscuiteer")
+#'
+NULL
+
+#' bsgc.hg19
+#'
+#' Dataset description
+#'
+#' @name bsgc.hg19
+#' @docType data
+#' @keywords data
+#' @usage data(bsgc.hg19, package="biscuiteer")
+#'
+NULL
+
+#' maps.hg19
+#'
+#' Dataset description
+#'
+#' @name maps.hg19
+#' @docType data
+#' @keywords data
+#' @usage data(maps.hg19, package="biscuiteer")
+#'
+NULL
+
+#' clocks
+#'
+#' Dataset description
+#'
+#' @name clocks
+#' @docType data
+#' @keywords data
+#' @usage data(clocks, package="biscuiteer")
+#'
+NULL
+
+#' openSeas.hg19
+#'
+#' Dataset description
+#'
+#' @name openSeas.hg19
+#' @docType data
+#' @keywords data
+#' @usage data(openSeas.hg19, package="biscuiteer")
+#'
+NULL
+
+#' openSeas.hg38
+#'
+#' Dataset description
+#'
+#' @name openSeas.hg38
+#' @docType data
+#' @keywords data
+#' @usage data(openSeas.hg38, package="biscuiteer")
+#'
+NULL
+
+#' seqinfo.hg19
+#'
+#' Dataset description
+#'
+#' @name seqinfo.hg19
+#' @docType data
+#' @keywords data
+#' @usage data(seqinfo.hg19, package="biscuiteer")
+#'
+NULL
+
+#' seqinfo.hg38
+#'
+#' Dataset description
+#'
+#' @name seqinfo.hg38
+#' @docType data
+#' @keywords data
+#' @usage data(seqinfo.hg38, package="biscuiteer")
+#'
+NULL
+
+#' seqinfo.mm10
+#'
+#' Dataset description
+#'
+#' @name seqinfo.mm10
+#' @docType data
+#' @keywords data
+#' @usage data(seqinfo.mm10, package="biscuiteer")
+#'
+NULL
